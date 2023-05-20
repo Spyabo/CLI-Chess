24655a57b34b5fdf01e7c7ca046b2604e55c1fbb
Spyabo <reesewright02@protonmail.com>
2023-05-20 13:15:38 +0100

    Add basic game loop

--- main.py
+++ main.py
@@ -1,7 +1,45 @@
 from board import Board, draw_board_black, draw_board_white
+import os
 
-x = Board.from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
-print(x.piece(0, 1).type)
-print(x.get_valid_moves(0, 1))
-x.piece(0, 1).move_to(0, 3)
-draw_board_white(x)
+
+def clear():
+    os.system("cls || clear")
+
+
+def newGame():
+    clear()
+    player1 = input("Enter name for White player: ")
+    player2 = input("Enter name for Black player: ")
+    turn = 0
+
+    x = Board.from_fen("RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rnbqkbnr")
+    while input != "q":
+        match turn:
+            case 0:
+                # White
+                draw_board_white(x)
+                move = input(f"{player1}, It's your turn!\n")
+
+            case 1:
+                # Black
+                draw_board_black(x)
+                move = input(f"{player2}, It's your turn!\n")
+                move[0]
+        turn = not turn
+
+
+def loadGame():
+    fen = input("Input the fen string of the game you would like to load ->")
+
+
+def main():
+    clear()
+    print("Welcome to CLI-Chess -> \n")
+    ans = input("1. Play New Game\n2. Load Game from FEN\n")
+    if ans == "1":
+        newGame()
+    elif ans == "2":
+        loadGame()
+
+
+main()
